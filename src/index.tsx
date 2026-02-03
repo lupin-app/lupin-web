64e9dbaa7cdc7389e420645a6a123de2d4b14372
KK <kennethkutyn@gmail.com>
2026-02-03 13:28:59 +0800

    adding Amplitude

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import './index.css';
 import React from "react";
 import { render } from "react-dom";
+import { init } from "@amplitude/analytics-browser";
 import { App } from "./App";
+
+declare global {
+  interface Window {
+    __AMPLITUDE_INITIALIZED__?: boolean;
+  }
+}
+
+if (!window.__AMPLITUDE_INITIALIZED__) {
+  init("76a066196b9500570577766e70e3895c", undefined, {
+    defaultTracking: {
+      pageViews: true,
+      sessions: true,
+      formInteractions: true,
+      fileDownloads: true,
+    },
+  });
+  window.__AMPLITUDE_INITIALIZED__ = true;
+}
 
 const faviconUrl = new URL("../lupin-logo-transparent.png", import.meta.url).href;
 const existingFavicon = document.querySelector<HTMLLinkElement>("link[rel*='icon']");
